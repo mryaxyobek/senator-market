edb60ad3ed8985c6b73183fef96fc6ea1182a18c
XabibullayevYaxyobek <123488242+mryaxyobek@users.noreply.github.com>
2024-02-04 22:10:12 +0500

    added top accounts section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import TopAccounts from '../components/TopAccounts';
 
 const Home = () => {
     return (
         <>
+            <TopAccounts />
         </>
     )
 };

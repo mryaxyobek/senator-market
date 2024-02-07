4d10bd8f2e8009d72e15a585b636943c77026fa2
XabibullayevYaxyobek <123488242+mryaxyobek@users.noreply.github.com>
2024-02-07 11:33:59 +0500

    added search filter buttons data

---
+++ src/assets/data.js
@@ -0,0 +1,66 @@
+export const searchFilterBtns = [
+    {
+        id: 0,
+        body: "Lednik"
+    },
+    {
+        id: 1,
+        body: "Shut"
+    },
+    {
+        id: 2,
+        body: "Joker"
+    },
+    {
+        id: 3,
+        body: "Old akakunt"
+    },
+    {
+        id: 4,
+        body: "70 lvl"
+    },
+    {
+        id: 5,
+        body: "Lambargumbar"
+    },
+    {
+        id: 6,
+        body: "Mumiyo"
+    },
+    {
+        id: 7,
+        body: "Top akkaunt"
+    },
+    {
+        id: 8,
+        body: "Koreski"
+    },
+    {
+        id: 9,
+        body: "Tesla"
+    },
+    {
+        id: 10,
+        body: "Blood raven"
+    },
+    {
+        id: 11,
+        body: "60 uc"
+    },
+    {
+        id: 12,
+        body: "Pagani"
+    },
+    {
+        id: 13,
+        body: "Aston martin"
+    },
+    {
+        id: 14,
+        body: "X-suit"
+    },
+    {
+        id: 15,
+        body: "Silvanus"
+    },
+]
